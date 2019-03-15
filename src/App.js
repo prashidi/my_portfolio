e20d19dc0e1601106f84a8ff722933395fd01843
test4 <test4@test4s-MacBook-Air.local>
2019-03-15 15:35:01 +0200

    cmmit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Projects from './Projects';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+    state = { displayBio: false}
 
+
+    toggleDisplayBio = () => {
+        this.setState({displayBio:!this.state.displayBio});
+    }
+
+    render () {
+
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Patrick Rashidi. I'm a full stack engineer.</p>
+                <p>I'm always looking forward to working on meanigfull projects.</p>
+                { 
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in Cape Town, and I love coding and the funny thing with me is I don't realy have friends because I code every day</p>
+                            <p>My favourite language is Python, but recently I dived into to JavaScript, and I think React.js is a awesome frontend framework</p>
+                            <p>Besides coding, also love music and soccer!</p>
+                            <div>
+                                <button onClick ={ this.toggleDisplayBio }>Show Less</button>
+                            </div>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick ={ this.toggleDisplayBio }>Read More</button>
+                        </div>
+                    )
+                }
+
+                <hr />
+                <Projects />
+            </div>
+        )
+    }
+}
 export default App;
---
+++ src/data/socialProfile.js
@@ -0,0 +1,29 @@
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+import emailIcon from '../assets/email_icon.png';
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'https://github.com/15Dkatz',
+    image: githubIcon
+  },
+  {
+    id: 2,
+    link: 'https://www.linkedin.com/in/david-katz-sf/',
+    image: linkedinIcon
+  },
+  {
+    id: 3,
+    link: 'https://twitter.com/dkcodehelper',
+    image: twitterIcon
+  },
+  {
+    id: 4,
+    link: 'mailto:dkcodehelper@gmail.com',
+    image: emailIcon
+  }
+];
+
+export default SOCIAL_PROFILES;
